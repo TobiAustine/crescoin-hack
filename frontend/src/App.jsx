d98457a5d55674a96ce26c21f8088ab623e281ba
ThatDivDoctor <autineosiene@gmail.com>
2023-11-26 13:27:55 +0100

    build

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import Signup from './pages/Signup'
 import Signin from './pages/Signin'
 import Forgotpassword from './pages/Forgotpassword'
+import About from './pages/About'
 
 function App() {
   
@@ -16,6 +17,7 @@ function App() {
       <Route path='/signup' element={<Signup/>}></Route> 
       <Route path='/signin' element={<Signin/>}></Route>
       <Route path='/forgotpassword' element={<Forgotpassword/>}></Route>
+      <Route path='/forgotpassword' element={<About/>}></Route>
     </Routes>
     
    
